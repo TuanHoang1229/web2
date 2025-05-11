6752a480417a6f0ae3b30e2eb3523c5e8156bb57
TuanHoang1229 <122627428+TuanHoang1229@users.noreply.github.com>
2025-05-11 17:38:14 +0700

    Update mk.py

--- mk.py
+++ mk.py
@@ -19,6 +19,13 @@ st.markdown(f"""
     <hr style="margin-top: 0;">
 """, unsafe_allow_html=True)
 
+
+st.image(
+    "https://scontent.fhan3-2.fna.fbcdn.net/v/t39.30808-6/304851178_540789087851004_9097165287000760892_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=gHy6kXCaw2AQ7kNvwE-PviU&_nc_oc=Admrg-I1Ihfs1OeVmhVMDCN2WjDhY-G92J9d7FWnfMrm3PH2DaIHFPtWsp3spMJ7lTA&_nc_zt=23&_nc_ht=scontent.fhan3-2.fna&_nc_gid=zUR1Tl_81MyfEa2IiZydTg&oh=00_AfJdAFQuQ6pb2wh4fVJ3RBHgdKJnQDRy9rhd0jvLBUbplQ&oe=6825F9BA",
+    use_container_width=True,
+    caption="📸 Trường THPT Lương Văn Tri"
+)
+
 # --- Tabs ---
 tabs = st.tabs([
     "🏠 Trang chủ",
@@ -29,13 +36,6 @@ tabs = st.tabs([
     "🧠 Trắc nghiệm",
     "💬 Góc chia sẻ",
 ])
-
-st.image(
-    "https://scontent.fhan3-2.fna.fbcdn.net/v/t39.30808-6/304851178_540789087851004_9097165287000760892_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=gHy6kXCaw2AQ7kNvwE-PviU&_nc_oc=Admrg-I1Ihfs1OeVmhVMDCN2WjDhY-G92J9d7FWnfMrm3PH2DaIHFPtWsp3spMJ7lTA&_nc_zt=23&_nc_ht=scontent.fhan3-2.fna&_nc_gid=zUR1Tl_81MyfEa2IiZydTg&oh=00_AfJdAFQuQ6pb2wh4fVJ3RBHgdKJnQDRy9rhd0jvLBUbplQ&oe=6825F9BA",
-    use_container_width=True,
-    caption="📸 Trường THPT Lương Văn Tri"
-)
-
 
 # --- Trang Chủ ---
 with tabs[0]:
